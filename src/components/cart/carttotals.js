06d76935f2c9dbd34ecdb2bd7a70a1da9546087f
itsnickjones <itsnickjones@yahoo.com>
2019-07-19 15:26:22 -0400

    commit 2

--- src/components/cart/carttotals.js
+++ src/components/cart/carttotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import PayPalButton from './paypalbutton';
 
-function CartTotals ({value}) {
+function CartTotals ({value,history}) {
     // state = {  }
     const {cartSubTotal,cartTax,cartTotal,clearCart}= value;
         return (
@@ -9,33 +10,35 @@ function CartTotals ({value}) {
                 <div className="container">
                     <div className="row">
                      <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
-                     <Link to="/">
-                        <button className="btn btn-outline-danger text-uppercase mb-3 px-5"
-                        type="button"
-                        onClick={()=>clearCart()}
-                        >
-                            clear cart
-                        </button>
-                     </Link>
-                     <h5>
-                         <span className="text-title">
-                         tax: 
-                         </span>
-                         <strong>$ {cartTax}</strong>
+                         <Link to="/">
+                            <button className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                            type="button"
+                            onClick={()=>clearCart()}
+                            >
+                                clear cart
+                            </button>
+                         </Link>
+                         <h5>
+                             <span className="text-title">
+                             tax: 
+                             </span>
+                             <strong>$ {cartTax}</strong>
 
-                     </h5>
-                     <h5>
-                         <span className="text-title">
-                         subtotal: 
-                         </span>
-                         <strong>$ {cartSubTotal}</strong>
-                     </h5>
-                     <h5>
-                         <span className="text-title">
-                         Total: 
-                         </span>
-                         <strong>$ {cartTotal}</strong>
-                     </h5>
+                         </h5>
+                         <h5>
+                             <span className="text-title">
+                             subtotal: 
+                             </span>
+                             <strong>$ {cartSubTotal}</strong>
+                         </h5>
+                         <h5>
+                             <span className="text-title">
+                             Total: 
+                             </span>
+                             <strong>$ {cartTotal}</strong>
+                         </h5>
+                         <PayPalButton total={cartTotal} clearCart={clearCart} history={history} >
+                         </PayPalButton>  
                      </div>
                     </div>
                 </div> 
